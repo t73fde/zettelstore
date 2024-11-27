3469119d72a63d59067dae245a50bb81b6ba245f
stern <ds@zettelstore.de>
2024-11-27 16:22:04 +0000

    Move previous new-style zids into internal package. They will probably be reused, when the indexer is rewritten.
    FossilOrigin-Name: b3a87e8180462dcd96423139d17c2e332b79387bbfede42bc3bc083a87ff6227

--- zettel/id/mapper/mapper.go
+++ zettel/id/mapper/mapper.go
@@ -12,7 +12,7 @@
 //-----------------------------------------------------------------------------
 
 // Package mapper provides a mechanism to map zettel identifier with 14 digits
-// to their successing identifier with four characters.
+// to identifier with four characters.
 package mapper
 
 import (
@@ -20,12 +20,105 @@ import (
 	"context"
 	"fmt"
 	"maps"
+	"strconv"
 	"sync"
 	"time"
 
+	"t73f.de/r/zsc/api"
 	"t73f.de/r/zsc/input"
 	"zettelstore.de/z/zettel/id"
 )
+
+// ----- Base36 zettel identifier.
+
+// ZidN is the internal identifier of a zettel. It is a number in the range
+// 1..36^4-1 (1..1679615), as it is externally represented by four alphanumeric
+// characters.
+type ZidN uint32
+
+// Some important ZettelIDs.
+const (
+	InvalidN = ZidN(0) // Invalid is a Zid that will never be valid
+)
+
+const maxZidN = 36*36*36*36 - 1
+
+// ParseUintN interprets a string as a possible zettel identifier
+// and returns its integer value.
+func ParseUintN(s string) (uint64, error) {
+	res, err := strconv.ParseUint(s, 36, 21)
+	if err != nil {
+		return 0, err
+	}
+	if res == 0 || res > maxZidN {
+		return res, strconv.ErrRange
+	}
+	return res, nil
+}
+
+// ParseN interprets a string as a zettel identification and
+// returns its value.
+func ParseN(s string) (ZidN, error) {
+	if len(s) != 4 {
+		return InvalidN, strconv.ErrSyntax
+	}
+	res, err := ParseUintN(s)
+	if err != nil {
+		return InvalidN, err
+	}
+	return ZidN(res), nil
+}
+
+// MustParseN tries to interpret a string as a zettel identifier and returns
+// its value or panics otherwise.
+func MustParseN(s api.ZettelID) ZidN {
+	zid, err := ParseN(string(s))
+	if err == nil {
+		return zid
+	}
+	panic(err)
+}
+
+// String converts the zettel identification to a string of 14 digits.
+// Only defined for valid ids.
+func (zid ZidN) String() string {
+	var result [4]byte
+	zid.toByteArray(&result)
+	return string(result[:])
+}
+
+// ZettelID return the zettel identification as a api.ZettelID.
+func (zid ZidN) ZettelID() api.ZettelID { return api.ZettelID(zid.String()) }
+
+// Bytes converts the zettel identification to a byte slice of 14 digits.
+// Only defined for valid ids.
+func (zid ZidN) Bytes() []byte {
+	var result [4]byte
+	zid.toByteArray(&result)
+	return result[:]
+}
+
+// toByteArray converts the Zid into a fixed byte array, usable for printing.
+//
+// Based on idea by Daniel Lemire: "Converting integers to fix-digit representations quickly"
+// https://lemire.me/blog/2021/11/18/converting-integers-to-fix-digit-representations-quickly/
+func (zid ZidN) toByteArray(result *[4]byte) {
+	d12 := uint32(zid) / (36 * 36)
+	d1 := d12 / 36
+	d2 := d12 % 36
+	d34 := uint32(zid) % (36 * 36)
+	d3 := d34 / 36
+	d4 := d34 % 36
+
+	const digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	result[0] = digits[d1]
+	result[1] = digits[d2]
+	result[2] = digits[d3]
+	result[3] = digits[d4]
+}
+
+// IsValid determines if zettel id is a valid one, e.g. consists of max. 14 digits.
+func (zid ZidN) IsValid() bool { return 0 < zid && zid <= maxZidN }
 
 // Mapper transforms old-style zettel identifier (14 digits) into new one (4 alphanums).
 //
@@ -35,13 +128,13 @@ import (
 // This will change in later versions.
 type Mapper struct {
 	fetcher   Fetcher
-	defined   map[id.Zid]id.ZidN // predefined mapping, constant after creation
-	mx        sync.RWMutex       // protect toNew ... nextZidN
-	toNew     map[id.Zid]id.ZidN // working mapping old->new
-	toOld     map[id.ZidN]id.Zid // working mapping new->old
-	nextZidM  id.ZidN            // next zid for manual
+	defined   map[id.Zid]ZidN // predefined mapping, constant after creation
+	mx        sync.RWMutex    // protect toNew ... nextZidN
+	toNew     map[id.Zid]ZidN // working mapping old->new
+	toOld     map[ZidN]id.Zid // working mapping new->old
+	nextZidM  ZidN            // next zid for manual
 	hadManual bool
-	nextZidN  id.ZidN // next zid for normal zettel
+	nextZidN  ZidN // next zid for normal zettel
 }
 
 // Fetcher is an object that will fetch all identifier currently in user.
@@ -52,62 +145,62 @@ type Fetcher interface {
 
 // Make creates a new Mapper.
 func Make(fetcher Fetcher) *Mapper {
-	defined := map[id.Zid]id.ZidN{
-		id.Invalid: id.InvalidN,
-		1:          id.MustParseN("0001"), // ZidVersion
-		2:          id.MustParseN("0002"), // ZidHost
-		3:          id.MustParseN("0003"), // ZidOperatingSystem
-		4:          id.MustParseN("0004"), // ZidLicense
-		5:          id.MustParseN("0005"), // ZidAuthors
-		6:          id.MustParseN("0006"), // ZidDependencies
-		7:          id.MustParseN("0007"), // ZidLog
-		8:          id.MustParseN("0008"), // ZidMemory
-		9:          id.MustParseN("0009"), // ZidSx
-		10:         id.MustParseN("000a"), // ZidHTTP
-		11:         id.MustParseN("000b"), // ZidAPI
-		12:         id.MustParseN("000c"), // ZidWebUI
-		13:         id.MustParseN("000d"), // ZidConsole
-		20:         id.MustParseN("000e"), // ZidBoxManager
-		21:         id.MustParseN("000f"), // ZidZettel
-		22:         id.MustParseN("000g"), // ZidIndex
-		23:         id.MustParseN("000h"), // ZidQuery
-		90:         id.MustParseN("000i"), // ZidMetadataKey
-		92:         id.MustParseN("000j"), // ZidParser
-		96:         id.MustParseN("000k"), // ZidStartupConfiguration
-		100:        id.MustParseN("000l"), // ZidRuntimeConfiguration
-		101:        id.MustParseN("000m"), // ZidDirectory
-		102:        id.MustParseN("000n"), // ZidWarnings
-		10100:      id.MustParseN("000s"), // Base HTML Template
-		10200:      id.MustParseN("000t"), // Login Form Template
-		10300:      id.MustParseN("000u"), // List Zettel Template
-		10401:      id.MustParseN("000v"), // Detail Template
-		10402:      id.MustParseN("000w"), // Info Template
-		10403:      id.MustParseN("000x"), // Form Template
-		10405:      id.MustParseN("000y"), // Delete Template
-		10700:      id.MustParseN("000z"), // Error Template
-		19000:      id.MustParseN("000q"), // Sxn Start Code
-		19990:      id.MustParseN("000r"), // Sxn Base Code
-		20001:      id.MustParseN("0010"), // Base CSS
-		25001:      id.MustParseN("0011"), // User CSS
-		40001:      id.MustParseN("000o"), // Generic Emoji
-		59900:      id.MustParseN("000p"), // Sxn Prelude
-		60010:      id.MustParseN("0012"), // zettel
-		60020:      id.MustParseN("0013"), // confguration
-		60030:      id.MustParseN("0014"), // role
-		60040:      id.MustParseN("0015"), // tag
-		90000:      id.MustParseN("0016"), // New Menu
-		90001:      id.MustParseN("0017"), // New Zettel
-		90002:      id.MustParseN("0018"), // New User
-		90003:      id.MustParseN("0019"), // New Tag
-		90004:      id.MustParseN("001a"), // New Role
+	defined := map[id.Zid]ZidN{
+		id.Invalid: InvalidN,
+		1:          MustParseN("0001"), // ZidVersion
+		2:          MustParseN("0002"), // ZidHost
+		3:          MustParseN("0003"), // ZidOperatingSystem
+		4:          MustParseN("0004"), // ZidLicense
+		5:          MustParseN("0005"), // ZidAuthors
+		6:          MustParseN("0006"), // ZidDependencies
+		7:          MustParseN("0007"), // ZidLog
+		8:          MustParseN("0008"), // ZidMemory
+		9:          MustParseN("0009"), // ZidSx
+		10:         MustParseN("000a"), // ZidHTTP
+		11:         MustParseN("000b"), // ZidAPI
+		12:         MustParseN("000c"), // ZidWebUI
+		13:         MustParseN("000d"), // ZidConsole
+		20:         MustParseN("000e"), // ZidBoxManager
+		21:         MustParseN("000f"), // ZidZettel
+		22:         MustParseN("000g"), // ZidIndex
+		23:         MustParseN("000h"), // ZidQuery
+		90:         MustParseN("000i"), // ZidMetadataKey
+		92:         MustParseN("000j"), // ZidParser
+		96:         MustParseN("000k"), // ZidStartupConfiguration
+		100:        MustParseN("000l"), // ZidRuntimeConfiguration
+		101:        MustParseN("000m"), // ZidDirectory
+		102:        MustParseN("000n"), // ZidWarnings
+		10100:      MustParseN("000s"), // Base HTML Template
+		10200:      MustParseN("000t"), // Login Form Template
+		10300:      MustParseN("000u"), // List Zettel Template
+		10401:      MustParseN("000v"), // Detail Template
+		10402:      MustParseN("000w"), // Info Template
+		10403:      MustParseN("000x"), // Form Template
+		10405:      MustParseN("000y"), // Delete Template
+		10700:      MustParseN("000z"), // Error Template
+		19000:      MustParseN("000q"), // Sxn Start Code
+		19990:      MustParseN("000r"), // Sxn Base Code
+		20001:      MustParseN("0010"), // Base CSS
+		25001:      MustParseN("0011"), // User CSS
+		40001:      MustParseN("000o"), // Generic Emoji
+		59900:      MustParseN("000p"), // Sxn Prelude
+		60010:      MustParseN("0012"), // zettel
+		60020:      MustParseN("0013"), // confguration
+		60030:      MustParseN("0014"), // role
+		60040:      MustParseN("0015"), // tag
+		90000:      MustParseN("0016"), // New Menu
+		90001:      MustParseN("0017"), // New Zettel
+		90002:      MustParseN("0018"), // New User
+		90003:      MustParseN("0019"), // New Tag
+		90004:      MustParseN("001a"), // New Role
 		// 100000000,   // Manual               -> 0020-00yz
-		9999999997:  id.MustParseN("00zx"), // ZidSession
-		9999999998:  id.MustParseN("00zy"), // ZidAppDirectory
-		9999999999:  id.MustParseN("00zz"), // ZidMapping
-		10000000000: id.MustParseN("0100"), // ZidDefaultHome
+		9999999997:  MustParseN("00zx"), // ZidSession
+		9999999998:  MustParseN("00zy"), // ZidAppDirectory
+		9999999999:  MustParseN("00zz"), // ZidMapping
+		10000000000: MustParseN("0100"), // ZidDefaultHome
 	}
 	toNew := maps.Clone(defined)
-	toOld := make(map[id.ZidN]id.Zid, len(toNew))
+	toOld := make(map[ZidN]id.Zid, len(toNew))
 	for o, n := range toNew {
 		if _, found := toOld[n]; found {
 			panic("duplicate predefined zid")
@@ -120,9 +213,9 @@ func Make(fetcher Fetcher) *Mapper {
 		defined:   defined,
 		toNew:     toNew,
 		toOld:     toOld,
-		nextZidM:  id.MustParseN("0020"),
+		nextZidM:  MustParseN("0020"),
 		hadManual: false,
-		nextZidN:  id.MustParseN("0101"),
+		nextZidN:  MustParseN("0101"),
 	}
 }
 
@@ -155,7 +248,7 @@ func (zm *Mapper) Warnings(ctx context.Context) (*id.Set, error) {
 }
 
 // LookupZidN returns the new-style identifier for a given old-style identifier.
-func (zm *Mapper) LookupZidN(zidO id.Zid) (id.ZidN, bool) {
+func (zm *Mapper) LookupZidN(zidO id.Zid) (ZidN, bool) {
 	if !zidO.IsValid() {
 		panic(zidO)
 	}
@@ -167,7 +260,7 @@ func (zm *Mapper) LookupZidN(zidO id.Zid) (id.ZidN, bool) {
 
 // AllocateZidN allocates a new new-style identifier, which is associated with
 // the given old-style identifier.
-func (zm *Mapper) AllocateZidN(zidO id.Zid) id.ZidN {
+func (zm *Mapper) AllocateZidN(zidO id.Zid) ZidN {
 	if zidN, found := zm.LookupZidN(zidO); found {
 		return zidN
 	}
@@ -200,7 +293,7 @@ func (zm *Mapper) AllocateZidN(zidO id.Zid) id.ZidN {
 }
 
 // LookupZidO returns the old-style identifier for a new-style identifier.
-func (zm *Mapper) LookupZidO(zidN id.ZidN) (id.Zid, bool) {
+func (zm *Mapper) LookupZidO(zidN ZidN) (id.Zid, bool) {
 	if zm != nil {
 		zm.mx.RLock()
 		zidO, found := zm.toOld[zidN]
@@ -325,11 +418,11 @@ func readZidO(inp *input.Input) id.Zid {
 	zidO, _ := id.Parse(string(inp.Src[pos:inp.Pos]))
 	return zidO
 }
-func readZidN(inp *input.Input) id.ZidN {
+func readZidN(inp *input.Input) ZidN {
 	pos := inp.Pos
 	for ('0' <= inp.Ch && inp.Ch <= '9') || ('a' <= inp.Ch && inp.Ch <= 'z') || ('A' <= inp.Ch && inp.Ch <= 'Z') {
 		inp.Next()
 	}
-	zidN, _ := id.ParseN(string(inp.Src[pos:inp.Pos]))
+	zidN, _ := ParseN(string(inp.Src[pos:inp.Pos]))
 	return zidN
 }
