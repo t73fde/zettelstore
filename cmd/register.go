c28c3b5dafa219d27f3a32e472244b18b2364ab7
stern <ds@zettelstore.de>
2025-12-19 17:17:32 +0000

    Update dependencies; fix lint regression
    FossilOrigin-Name: fe0eaadc70874062bc59cc452b216ba74af70294906bda114474fd13ec2f0e3f

--- cmd/register.go
+++ cmd/register.go
@@ -15,9 +15,9 @@ package cmd
 
 // Mention all needed boxes to have them registered.
 import (
-	_ "zettelstore.de/z/internal/box/compbox"
-	_ "zettelstore.de/z/internal/box/constbox"
-	_ "zettelstore.de/z/internal/box/dirbox"
-	_ "zettelstore.de/z/internal/box/filebox"
-	_ "zettelstore.de/z/internal/box/membox"
+	_ "zettelstore.de/z/internal/box/compbox"  // Make computed box available
+	_ "zettelstore.de/z/internal/box/constbox" // Make box with constant zettel available
+	_ "zettelstore.de/z/internal/box/dirbox"   // Standard zettel box
+	_ "zettelstore.de/z/internal/box/filebox"  // File-based box, for example zip file
+	_ "zettelstore.de/z/internal/box/membox"   // In-memory box
 )
